4328d97762a9089f19c9db72920defe1ceb14e94
SanJiang <changsanjiang@gmail.com>
2018-06-28 14:18:01 +0800

    观察recorder的变更

--- Demo/SJAttributesFactory/ViewController.m
+++ Demo/SJAttributesFactory/ViewController.m
@@ -229,6 +229,26 @@ static NSString *UITableViewCellID = @"UITableViewCell";
             });
         }
             break;
+        case 8: {
+            tips = @"测试";
+            
+            attr = sj_makeAttributesString(^(SJAttributeWorker * _Nonnull make) {
+//                大写 P 表示 Unicode 字符集七个字符属性之一：标点字符
+//                L：字母；
+//                M：标记符号（一般不会单独出现）；
+//                Z：分隔符（比如空格、换行等）；
+//                S：符号（比如数学符号、货币符号等）；
+//                N：数字（比如阿拉伯数字、罗马数字等）；
+//                C：其他字符
+                
+                make.append(@"&&&&###@@$$$$");
+                
+                make.regexp(@"^[\\p{S}|\\p{P}|\\p{M}|\\p{Z}]+$", ^(SJAttributesRangeOperator * _Nonnull make) {
+                    make.textColor([UIColor redColor]);
+                });
+            });
+        }
+            break;
     }
     
     
--- SJAttributesFactory.podspec
+++ SJAttributesFactory.podspec
@@ -16,7 +16,7 @@ Pod::Spec.new do |s|
   #
 
   s.name         = "SJAttributesFactory"
-  s.version      = "2.0.3.2"
+  s.version      = "2.0.3.3"
   s.summary      = "富文本编辑工厂."
 
   # This description is used to generate tags and improve search results.
